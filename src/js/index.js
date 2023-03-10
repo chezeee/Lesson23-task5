213303d109d3a720c38f62de0d414283da35532c
Ilya Yalymov <chezeee@gmail.com>
2023-03-10 20:53:33 +0300

    Initial structure added

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string[]} items
+ */
+const renderShoppingList = (items) => {
+  // your code
+};
+
+// Sample usage - do not modify
+const sampleList = ["Orange", "Banana", "Coffee", "Paper"];
+renderShoppingList(sampleList);
